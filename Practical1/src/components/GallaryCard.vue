a2cd85c2b26923db5f0d5e197450ee0659f8637b
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-03-28 14:12:32 +0530

    added center for all opt in center

--- Practical1/src/components/GallaryCard.vue
+++ Practical1/src/components/GallaryCard.vue
@@ -1,19 +1,21 @@
 <template>
-  <div v-for="item in fulldata" :key="item.id" class="main_container">
-    <h2>{{ item.heading }}</h2>
+  <center>
+    <div v-for="item in fulldata" :key="item.id" class="main_container">
+      <h2>{{ item.heading }}</h2>
 
-    <div class="hola_image"><img :src="item.image" alt="image of car" /></div>
+      <div class="hola_image"><img :src="item.image" alt="image of car" /></div>
 
-    <h3 class="details_container">{{ item.details }}</h3>
+      <h3 class="details_container">{{ item.details }}</h3>
 
-    <div>
-      <h3 class="details_container">{{ item.specs }}</h3>
+      <div>
+        <h3 class="details_container">{{ item.specs }}</h3>
+      </div>
+
+      <button @click="clickHandler(item.price, item.heading)" class="info_btn">
+        info
+      </button>
     </div>
-
-    <button @click="clickHandler(item.price, item.heading)" class="info_btn">
-      info
-    </button>
-  </div>
+  </center>
 </template>
 
 <script>
@@ -36,31 +38,30 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
+h3 {
+  margin: 40px 0 0;
+}
 
-  .btn {
-    border-radius: 15px;
-    padding: 10px;
-    background-color: #2c3e50;
-    color: #fff;
-  }
-  .main_container {
-    background-color: burlywood;
-    padding: 50px;
-    max-width: 400px;
-    margin: 10px;
-  }
-  .info_btn {
-    border-radius: 15px;
-    padding: 10px 25px;
-    margin: 10px 0 0 0;
-    background-color: #50302c;
-    color: #fff;
-  }
-  img{
-    max-width: 400px  ;
-  }
-
+.btn {
+  border-radius: 15px;
+  padding: 10px;
+  background-color: #2c3e50;
+  color: #fff;
+}
+.main_container {
+  background-color: burlywood;
+  padding: 50px;
+  max-width: 400px;
+  margin: 10px;
+}
+.info_btn {
+  border-radius: 15px;
+  padding: 10px 25px;
+  margin: 10px 0 0 0;
+  background-color: #50302c;
+  color: #fff;
+}
+img {
+  max-width: 400px;
+}
 </style>
