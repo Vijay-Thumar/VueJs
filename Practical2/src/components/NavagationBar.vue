cbeb6d1074793038a357880ee727f51264320a61
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-03-30 09:37:32 +0530

    initial commit

---
+++ Practical2/src/components/NavagationBar.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="Navagation_bar">
+
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.Navagation_bar{
+    min-width: 100%;
+    color: aliceblue;
+    
+}
+</style>
