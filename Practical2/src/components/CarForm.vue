3134b6f9a136915e1876a50300a7a62803854d4b
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-04-18 20:50:21 +0530

    final push

---
+++ Practical2/src/components/CarForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <input v-model="heading" placeholder="Enter Heading" />
+    <input v-model="details" placeholder="Enter Details" />
+    <input v-model="image" placeholder="Enter Image Url" />
+    <input v-model="price" placeholder="Enter Price of car" />
+    <button type="submit" @click="formSubmitHandler">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      heading: null,
+      details: null,
+      image: null,
+      price: null,
+    };
+  },
+  methods(){
+      formSubmitHandler(e){}
+  },
+};
+</script>
+
+<style></style>
--- Practical2/src/components/NavagationBar.vue
+++ Practical2/src/components/NavagationBar.vue
@@ -1,17 +1,59 @@
 <template>
-<div class="Navagation_bar">
+  <div class="Navagation_bar">
+    <div>Navagation bar from navagation</div>
+      <div class="add_button" @click="addButtonHandler"> Add </div>
 
-</div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-.Navagation_bar{
-    min-width: 100%;
-    color: aliceblue;
-    
+.Navagation_bar {
+  min-width: 80%;
+  background-color: rgb(84, 121, 241);
+  height: 55px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: x-large;
+  font-weight: 600;
+  border-radius: 50px;
+  padding: 0 80px;
+
+}
+.add_button{
+  background-color: green;
+  border-radius: 25px;
+  padding: 8px 15px;
+  font-size: 21px;
+  color: white;
+  cursor: pointer;
+}
+.add_button:hover{
+  background-color: green;
+  border-radius: 25px;
+  border-top:2px solid darkgreen;
+  border-top:2px solid darkgreen;
+  padding: 8px 15px;
+  font-size: 21px;
+  color: white;
+  cursor: pointer;
+}
+.add_button:active {
+  background-color: green;
+  border-radius: 25px;
+  border-top:1px solid darkgreen;
+  border-top:1px solid darkgreen;
+  padding: 8px 15px;
+  font-size: 21px;
+  color: white;
+  cursor: pointer;
 }
 </style>
