9722ec55d1c38df2cac5987157fb7d268d6e4451
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-04-21 15:54:10 +0530

    final push

---
+++ Practical3/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="background-color: white; color: black">
+    <Homecomponent />
+  </div>
+</template>
+
+<script>
+import Homecomponent from "./components/HomeComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    Homecomponent,
+  },
+};
+</script>
